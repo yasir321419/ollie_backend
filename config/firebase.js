57e1eb93f0358e2318a98efcb3448afb64271ed7
yasir321419 <yasir@chameleon-ideas.com>
2025-08-21 00:20:26 +0500

    changes

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-// const serviceAccount = require('../ollie-f122b-firebase-adminsdk-fbsvc-7d292030e9.json'); // adjust filename if needed
+const serviceAccount = require('../ollie-f122b-firebase-adminsdk-fbsvc-7d292030e9.json'); // adjust filename if needed
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
