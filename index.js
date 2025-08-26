541538f2984fe8af266cb3f5f631e4fa058c3486
yasir321419 <yasir@chameleon-ideas.com>
2025-08-27 02:20:06 +0500

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
-const API_PRIFEX = '/api/v1';  // Prefix for all routes
+const API_PRIFEX = '/api/v1/';  // Prefix for all routes
 const rootRouter = require("./routes/index");
 const globalErrorMiddleware = require("./middleware/globalMiddleware");
 const dbConnect = require('./db/connectivity');
